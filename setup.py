fd02b26ecb99b8a9fc7d652813f03844a535afac
orangetin <abhy@together.xyz>
2023-05-30 20:55:34 -0700

    updated format

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='together',
+    name="together",
     author="togethercomputer",
     author_email="support@together.xyz",
     description="Together cli is a tool to help you join together computer",
-    version='0.0.84',
+    version="0.0.84",
     scripts=["together/bin/together"],
-    package_dir={'together': 'together'},
+    package_dir={"together": "together"},
     packages=find_packages(),
     install_requires=[
         "typer",
@@ -15,6 +15,6 @@ setup(
         "rich",
         "loguru",
         "huggingface-hub",
-        "pynvml"
-    ]
+        "pynvml",
+    ],
 )
