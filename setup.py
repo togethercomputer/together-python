8bf846e9346831a3a37d6974c0e05e686c3e6442
orangetin <abhy@together.xyz>
2023-05-30 20:53:02 -0700

    updated structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='together',
+    author="togethercomputer",
+    author_email="support@together.xyz",
+    description="Together cli is a tool to help you join together computer",
+    version='0.0.84',
+    scripts=["together/bin/together"],
+    package_dir={'together': 'together'},
+    packages=find_packages(),
+    install_requires=[
+        "typer",
+        "requests",
+        "rich",
+        "loguru",
+        "huggingface-hub",
+        "pynvml"
+    ]
+)
