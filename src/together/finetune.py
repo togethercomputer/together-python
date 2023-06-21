b2b5e9d57f7691d6425deef1751a4e9a347e7c6f
orangetin <abhy@together.xyz>
2023-06-21 14:21:55 -0700

    Updated CLI hyperparameters for fine tuning

--- src/together/finetune.py
+++ src/together/finetune.py
@@ -1,7 +1,7 @@
 import os
 import posixpath
 import urllib.parse
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, Optional
 
 import requests
 
@@ -30,14 +30,14 @@ class Finetune:
         training_file: str,  # training file_id
         validation_file: Optional[str] = None,  # validation file_id
         model: Optional[str] = None,
-        n_epochs: Optional[int] = 4,
-        batch_size: Optional[int] = None,
-        learning_rate_multiplier: Optional[float] = None,
-        prompt_loss_weight: Optional[float] = 0.01,
-        compute_classification_metrics: Optional[bool] = False,
-        classification_n_classes: Optional[int] = None,
-        classification_positive_class: Optional[str] = None,
-        classification_betas: Optional[List[Any]] = None,
+        n_epochs: Optional[int] = 1,
+        batch_size: Optional[int] = 32,
+        learning_rate: Optional[float] = 0.00001,
+        warmup_steps: Optional[int] = 0,
+        train_warmup_steps: Optional[int] = 0,
+        seq_length: Optional[int] = 2048,
+        seed: Optional[int] = 42,
+        fp16: Optional[bool] = True,
         suffix: Optional[str] = None,
     ) -> Dict[Any, Any]:
         parameter_payload = {
@@ -46,12 +46,12 @@ class Finetune:
             "model": model,
             "n_epochs": n_epochs,
             "batch_size": batch_size,
-            "learning_rate_multiplier": learning_rate_multiplier,
-            "prompt_loss_weight": prompt_loss_weight,
-            "compute_classification_metrics": compute_classification_metrics,
-            "classification_n_classes": classification_n_classes,
-            "classification_positive_class": classification_positive_class,
-            "classification_betas": classification_betas,
+            "learning_rate": learning_rate,
+            "warmup_steps": warmup_steps,
+            "train_warmup_steps": train_warmup_steps,
+            "seq_length": seq_length,
+            "seed": seed,
+            "fp16": fp16,
             "suffix": suffix,
         }
 
