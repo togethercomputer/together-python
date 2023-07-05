fa6cd6868e38e5051f102b503a178d8b803d365e
orangetin <abhy@together.xyz>
2023-07-05 14:41:09 -0700

    Temporary R2 redirect

--- src/together/files.py
+++ src/together/files.py
@@ -5,6 +5,7 @@ import urllib.parse
 from typing import Dict, List, Optional, Union
 
 import requests
+import sys
 from tqdm import tqdm
 
 
@@ -58,32 +59,43 @@ class Files:
         return response_json
 
     def upload_file(self, file: str) -> Dict[str, Union[str, int]]:
-        data = {"purpose": "fine-tune"}
+        data = {"purpose": "fine-tune", "file_name": os.path.basename(file)}
+        print(data)
         headers = {
             "Authorization": f"Bearer {self.together_api_key}",
         }
 
         if not validate_json(file=file):
             raise ValueError("Could not load file: invalid .jsonl file detected.")
-
+        # TEMP_URL = "https://47b1b605b8ff0371ec661ebaf3b433c1.r2.cloudflarestorage.com/together-dev//finetune/file-e5731dc6-7e46-4f3a-95c9-4864bd8e19ed?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=257bc5a86d96c8b95a6521e4224f0b18%2F20230705%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20230705T205049Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=9b30c8d69abb9eb959e7b90aa71577af4ff337634f1e7fe5f48d919e701ba889"
+        session = requests.Session()
         # send request
-        try:
-            with open(file, "rb") as f:
-                response = requests.post(
-                    self.endpoint_url, headers=headers, files={"file": f}, data=data
-                )
-
-        except requests.exceptions.RequestException as e:
-            raise ValueError(f"Error raised by inference endpoint: {e}")
-
-        try:
-            response_json = dict(response.json())
-        except Exception:
-            raise ValueError(
-                f"JSON Error raised. \nResponse status code: {str(response.status_code)}"
+        # try:
+        response1 = session.post(
+                self.endpoint_url, data=data, headers=headers,
             )
+        print(response1.text)
+        print("Uploading file...")
+        r2_signed_url = response1.headers['Location']
+        with open(file, "rb") as f:
+            response = session.put(
+                r2_signed_url, files={"file": f}
+            )
+        # headers=headers, data=data
 
-        return response_json
+        # except (requests.exceptions.RequestException, requests.exceptions.SSLError) as e:
+        #     print(f"Error raised by inference endpoint: {e}")
+        #     sys.exit(0)
+
+        # try:
+        #     response_json = dict(response.json())
+        # except Exception:
+        print(response.text)
+        #     raise ValueError(
+        #         f"JSON Error raised. \nResponse status code: {str(response.status_code)}"
+        #     )
+
+        # return response_json
 
     def delete_file(self, file_id: str) -> Dict[str, str]:
         delete_url = urllib.parse.urljoin(self.endpoint_url, file_id)
