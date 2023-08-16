b20e4c21314dd1d19230da2d4670d817b803487a
Carson Lam <carsonlam@Carsons-MBP-2.hsd1.ca.comcast.net>
2023-08-16 13:26:58 -0700

    type fix

--- src/together/files.py
+++ src/together/files.py
@@ -277,7 +277,7 @@ class Files:
 def check_json(
     file: str,
     model: str = None,
-) -> Dict[str, Union[str, int, bool, list, object]]:
+) -> Dict[str, Union[str, int, bool, list, dict]]:
     report_dict = {"is_check_passed": True, "error_list": []}
 
     eos_token = None
