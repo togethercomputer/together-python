5471260cc27cb8e761c2b14d1e1aeae7e33470d2
Justus Calvin <justus@together.xyz>
2023-09-20 14:16:50 -0700

    Check for batch size set to none and set to 32 if not set.

--- src/together/finetune.py
+++ src/together/finetune.py
@@ -98,7 +98,9 @@ class Finetune:
                 f"Batch size must be 144 for {model} model. Please set batch size to 144"
             )
         
-        if batch_size < 4:
+        if batch_size is None:
+            batch_size = 32
+        elif batch_size < 4:
             raise ValueError(f"Batch size must be >= 4.")
 
         # TODO: REMOVE THIS CHECK WHEN WE HAVE CHECKPOINTING WORKING FOR 70B models
