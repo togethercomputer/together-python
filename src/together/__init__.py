d4af97a6587158fbab79bae3df7393925eae3b77
Carson Lam <carsonlam@Carsons-MacBook-Pro-2.local>
2023-09-22 16:48:49 -0700

    fix circular import

--- src/together/__init__.py
+++ src/together/__init__.py
@@ -26,7 +26,12 @@ default_text_model = "togethercomputer/RedPajama-INCITE-7B-Chat"
 default_image_model = "runwayml/stable-diffusion-v1-5"
 log_level = "WARNING"
 
-from .utils.utils import get_logger, verify_api_key, extract_time, parse_timestamp  # noqa
+from .utils.utils import (
+    get_logger,
+    verify_api_key,
+    extract_time,
+    parse_timestamp,
+)  # noqa
 from .models import Models
 from .complete import Complete
 from .error import *
