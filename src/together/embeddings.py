ff9f38ba34d118237cb29ec0963e6e5a28d86e55
Carson Lam <carsonlam@Carsons-MBP-2.attlocal.net>
2023-12-02 16:14:03 -0800

    embeddings take str and list of strings
    this is to make the method compatible with openai

--- src/together/embeddings.py
+++ src/together/embeddings.py
@@ -1,5 +1,5 @@
-from typing import Any, Dict, Optional, Union, List
 import concurrent.futures
+from typing import Any, Dict, List, Optional, Union
 
 import together
 from together.utils import create_post_request, get_logger
@@ -15,12 +15,10 @@ class embeddings:
         input: Union[str, List[str]],
         model: Optional[str] = "",
     ) -> Dict[str, Any]:
-
         if model == "":
             model = together.default_embedding_model
 
         if isinstance(input, str):
-
             parameter_payload = {
                 "input": input,
                 "model": model,
@@ -29,14 +27,11 @@ class embeddings:
             return cls._process_input(parameter_payload)
 
         elif isinstance(input, list):
-
-            response = {"object":"list","data":[],"model":model,"request_id":""}
+            response = {"object": "list", "data": [], "model": model, "request_id": ""}
 
             # If input is a list, process each string concurrently
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                parameter_payloads = [
-                    {"input": item, "model": model} for item in input
-                ]
+                parameter_payloads = [{"input": item, "model": model} for item in input]
                 results = list(executor.map(cls._process_input, parameter_payloads))
 
             response["data"] = [r["data"][0] for r in results]
@@ -46,7 +41,6 @@ class embeddings:
 
     @classmethod
     def _process_input(cls, parameter_payload: Dict[str, Any]) -> Dict[str, Any]:
-
         # send request
         response = create_post_request(
             url=together.api_base_embeddings, json=parameter_payload
@@ -59,5 +53,3 @@ class embeddings:
             raise together.JSONError(e, http_status=response.status_code)
 
         return response_json
-
-
