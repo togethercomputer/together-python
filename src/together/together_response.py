f7baf6080d10f8df158f93409ce201a9934c2471
orangetin <abhy@together.ai>
2024-03-07 14:29:59 -0800

    fix typing in TogetherResponse

--- src/together/together_response.py
+++ src/together/together_response.py
@@ -7,13 +7,13 @@ class TogetherResponse:
         self.data = data
 
     @property
-    def request_id(self) -> Optional[str]:
+    def request_id(self) -> str | None:
         if "cf-ray" in self._headers:
             return str(self._headers["cf-ray"])
         return None
 
     @property
-    def requests_remaining(self) -> Optional[int]:
+    def requests_remaining(self) -> int | None:
         """
         Number of requests remaining at current rate limit
         """
@@ -22,13 +22,13 @@ class TogetherResponse:
         return None
 
     @property
-    def processed_by(self) -> Optional[str]:
+    def processed_by(self) -> str | None:
         if "x-hostname" in self._headers:
             return str(self._headers["x-hostname"])
         return None
 
     @property
-    def response_ms(self) -> Optional[int]:
+    def response_ms(self) -> int | None:
         if "x-total-time" in self._headers:
             h = self._headers["x-total-time"]
             return None if h is None else round(float(h))
