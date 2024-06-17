841a5ac95d507388af71b498ecf7a9f813a99a84
orangetin <abhy@together.ai>
2024-06-17 14:39:14 -0700

    update test models

--- tests/integration/constants.py
+++ tests/integration/constants.py
@@ -1,6 +1,7 @@
 completion_test_model_list = [
     "meta-llama/Llama-2-7b-hf",
-    "togethercomputer/StripedHyena-Hessian-7B",
+    "meta-llama/Llama-2-8b-hf",
+    "mistralai/Mistral-7B-Instruct-v0.3",
 ]
 chat_test_model_list = []
 embedding_test_model_list = []
