b30270061e253066fef9b62d2ba6e0ac5d8e5889
Artem Chumachenko <artek.chumak@gmail.com>
2024-09-23 17:51:21 +0200

    style

--- src/together/cli/api/utils.py
+++ src/together/cli/api/utils.py
@@ -6,7 +6,9 @@ from typing import Literal
 class AutoIntParamType(click.ParamType):
     name = "integer"
 
-    def convert(self, value: str, param: click.Parameter | None, ctx: click.Context | None) -> int | Literal["auto"] | None:
+    def convert(
+        self, value: str, param: click.Parameter | None, ctx: click.Context | None
+    ) -> int | Literal["auto"] | None:
         if isinstance(value, int):
             return value
 
